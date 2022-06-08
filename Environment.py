0996bb82862a1c7bafc3225e5da2d60badc83a44
Ehsan Hamzei <ehsan.hamzei@unimelb.edu.au>
2022-06-08 17:25:02 +1000

    Add test scenario

--- Environment.py
+++ Environment.py
@@ -221,69 +221,73 @@ if __name__ == '__main__':
 
     # create view graph
     vgs, isovist_objects = ie.construct_view_graph()
-    cidx = ie.containers_names.index('Active Hub')
-    vg = vgs[cidx]
-    isovist_object = isovist_objects[cidx]
+    if not Parameters.basic and not Parameters.hypo:
+        cidx = ie.containers_names.index('Active Hub')
+        vg = vgs[cidx]
+        isovist_object = isovist_objects[cidx]
 
-    # calculate shortest path and generate verbal description
-    vp, pv = vg.shortest_path_regions(0, len(vg.regions_list) - 1)
+        # calculate shortest path and generate verbal description
+        vp, pv = vg.shortest_path_regions(0, len(vg.regions_list) - 1)
 
-    # derive door-to-door visibility graph (doors and decision points)
-    connected, dtd_graph = vg.generate_door_to_door_graph(isovist_object)
+        # derive door-to-door visibility graph (doors and decision points)
+        connected, dtd_graph = vg.generate_door_to_door_graph(isovist_object)
 
-    # derive door-to-door visibility graph (only doors)
-    connected2, dtd_graph2 = vg.generate_door_to_door_graph(isovist_object, only_doors=True)
+        # derive door-to-door visibility graph (only doors)
+        connected2, dtd_graph2 = vg.generate_door_to_door_graph(isovist_object, only_doors=True)
 
-    # derive all shortest path visibility graph and spanning tree
-    vps, pvs, st_vps, st_pvs, nvgraph = \
-        vg.generate_navigation_graph(isovist_object, indirect_access=False)
+        # derive all shortest path visibility graph and spanning tree
+        vps, pvs, st_vps, st_pvs, nvgraph = \
+            vg.generate_navigation_graph(isovist_object, indirect_access=False)
 
-    # derive place graph
-    place_graph = vg.generate_place_graph(isovist_object)
+        # derive place graph
+        place_graph = vg.generate_place_graph(isovist_object)
 
-    input('Press Enter: Describe the shortest path')
-    plotter = Plotter()
-    plotter.add_isovist(isovist_object)
-    plotter.add_views(pv)
-    plotter.show()
-    plotter.close()
-    vg.generate_route_description(vp)
-
-    input('Press Enter: Door to door visibility (doors+gateways)')
-    plotter = Plotter()
-    plotter.add_isovist(isovist_object)
-    plotter.add_points_lines(connected)
-    plotter.show()
-    plotter.close()
-    plotter.write_graph('d-t-d-all.html', dtd_graph, is_directed=False)
-
-    input('Press Enter: Door to door visibility (only doors)')
-    plotter = Plotter()
-    plotter.add_poly(isovist_object.space_x, isovist_object.space_y)
-    plotter.add_holes(isovist_object.holes_x, isovist_object.holes_y)
-    plotter.add_points(isovist_object.door_points[:isovist_object.door_idx], 'doors')
-    plotter.add_points_lines(connected2)
-    plotter.show()
-    plotter.close()
-    plotter.write_graph('d-t-d-doors.html', dtd_graph2, is_directed=False)
-
-    input('Press Enter: Portal-junction navigation graph')
-    plotter = Plotter()
-    plotter.add_isovist(isovist_object)
-
-    for pv in pvs:
+        input('Press Enter: Describe the shortest path')
+        plotter = Plotter()
+        plotter.add_isovist(isovist_object)
         plotter.add_views(pv)
-    plotter.show()
+        plotter.show()
+        plotter.close()
+        vg.generate_route_description(vp)
 
-    plotter.refresh()
-    for pv in st_pvs:
-        plotter.add_views(pv)
-    plotter.show()
+        input('Press Enter: Door to door visibility (doors+gateways)')
+        plotter = Plotter()
+        plotter.add_isovist(isovist_object)
+        plotter.add_points_lines(connected)
+        plotter.show()
+        plotter.close()
+        plotter.write_graph('d-t-d-all.html', dtd_graph, is_directed=False)
 
-    plotter.refresh()
-    for pv in st_pvs:
-        plotter.add_points_lines(pv, is_vis=False)
-    plotter.show()
+        input('Press Enter: Door to door visibility (only doors)')
+        plotter = Plotter()
+        plotter.add_poly(isovist_object.space_x, isovist_object.space_y)
+        plotter.add_holes(isovist_object.holes_x, isovist_object.holes_y)
+        plotter.add_points(isovist_object.door_points[:isovist_object.door_idx], 'doors')
+        plotter.add_points_lines(connected2)
+        plotter.show()
+        plotter.close()
+        plotter.write_graph('d-t-d-doors.html', dtd_graph2, is_directed=False)
 
-    input('Press Enter: Place graph generation; visualize for all and only for landmark 2')
-    plotter.write_graph('placegraph.html', place_graph)
+        input('Press Enter: Portal-junction navigation graph')
+        plotter = Plotter()
+        plotter.add_isovist(isovist_object)
+
+        for pv in pvs:
+            plotter.add_views(pv)
+        plotter.show()
+
+        plotter.refresh()
+        for pv in st_pvs:
+            plotter.add_views(pv)
+        plotter.show()
+
+        plotter.refresh()
+        for pv in st_pvs:
+            plotter.add_points_lines(pv, is_vis=False)
+        plotter.show()
+
+        input('Press Enter: Place graph generation; visualize for all and only for landmark 2')
+        plotter.write_graph('placegraph.html', place_graph)
+    else:
+        vg = vgs[0]
+        isovist_object = isovist_objects[0]
